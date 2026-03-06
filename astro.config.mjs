50f5abccc1f487e6b5151656ed52e96e4e5642df
Atdunbg <atdunbg@qq.com>
2026-03-06 10:58:45 +0800

    change config site to atdunbg.xyz

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://atdunbg.github.io/",
+	site: "https://atdunbg.xyz",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
